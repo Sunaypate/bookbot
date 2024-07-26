028dc26a984bffb8fd6195bb37ab2b81cd51daa2
Sunay P <sunayp2048@gmail.com>
2024-07-26 18:16:57 -0400

    added dictionary sort (currently broken)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = word_counter(book_text)
     print(f"The amount of words in this book are {word_count}")
-    character_counter(book_text)
+    dict_sorter(character_counter_fast(book_text))
 
 
 def get_book_text(filepath):
@@ -13,26 +13,30 @@ def get_book_text(filepath):
 def word_counter(file_contents):
     return len(file_contents.split())
 
-def character_counter(string):
-    character_dict = {}
-    for character in string:
-        character_dict[character] = 0
-    for letter in string:
-        temp = character_dict[letter]
-        temp += 1
-        character_dict[letter] = temp
-    
-    # print(character_dict)
 
 def character_counter_fast(string):
+    low_string = string.lower()
     character_dict = {}
-    for character in string:
+    for character in low_string:
         if character in character_dict:
             temp = character_dict[character]
             temp += 1
             character_dict[character] = temp
         else:
             character_dict[character] = 1
+    return character_dict
 
-    
+def dict_sorter(dict):
+    unsorted_keys = []
+    sorted_keys = []
+    min = ""
+    for key in dict:
+        unsorted_keys.append(key)
+    high = max(unsorted_keys)
+    for key in range(0, len(unsorted_keys)):
+        for character in range(0, len(unsorted_keys)):
+            if unsorted_keys[key] < high:
+                min = unsorted_keys[key]
+        sorted_keys.append(unsorted_keys[key])
+        print(sorted_keys)
 main()
