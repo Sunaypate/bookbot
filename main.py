b0bcb5d6254d8e7c788bee420f345444669abbbb
Sunay P <sunayp2048@gmail.com>
2024-07-26 17:00:58 -0400

    created basic counter (include up case)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = word_counter(book_text)
     print(f"The amount of words in this book are {word_count}")
-    
+    character_counter_fast(book_text)
 
 
 def get_book_text(filepath):
@@ -13,4 +13,21 @@ def get_book_text(filepath):
 def word_counter(file_contents):
     return len(file_contents.split())
 
+def character_counter(string):
+    character_dict = {}
+    for character in string:
+        character_dict[character] = 1
+    for letter in string:
+        temp = character_dict[letter]
+        temp += 1
+        character_dict[letter] = temp
+    
+    print(character_dict)
+
+def character_counter_fast(string):
+    character_dict = {}
+    for character in string:
+        character_dict[character] = 1 
+    print(character_dict)
+    
 main()
