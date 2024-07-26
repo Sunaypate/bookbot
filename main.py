b966775f65ae4c0924f63c66e5fe3a24c08b1b5c
Sunay P <sunayp2048@gmail.com>
2024-07-26 17:40:46 -0400

    Revamped character counter for speed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = word_counter(book_text)
     print(f"The amount of words in this book are {word_count}")
-    character_counter_fast(book_text)
+    character_counter(book_text)
 
 
 def get_book_text(filepath):
@@ -16,18 +16,23 @@ def word_counter(file_contents):
 def character_counter(string):
     character_dict = {}
     for character in string:
-        character_dict[character] = 1
+        character_dict[character] = 0
     for letter in string:
         temp = character_dict[letter]
         temp += 1
         character_dict[letter] = temp
     
-    print(character_dict)
+    # print(character_dict)
 
 def character_counter_fast(string):
     character_dict = {}
     for character in string:
-        character_dict[character] = 1 
-    print(character_dict)
+        if character in character_dict:
+            temp = character_dict[character]
+            temp += 1
+            character_dict[character] = temp
+        else:
+            character_dict[character] = 1
+
     
 main()
